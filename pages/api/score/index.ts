7e91441c5a3c826ea26da2f136e482f6b696b00c
Duong Nguyen <duongital@gmail.com>
2022-08-07 19:07:23 +0700

    fix: not load xlsx on prd

--- pages/api/score/index.ts
+++ pages/api/score/index.ts
@@ -1,24 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import xlsx from "node-xlsx";
 
-import fs from "fs";
 import path from "path";
-import getConfig from "next/config";
-const { serverRuntimeConfig } = getConfig();
+import fs from "fs";
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const data = xlsx.parse(
-    fs.readFileSync(
-      path.join(serverRuntimeConfig.PROJECT_ROOT, "pages/api/score/data.xlsx")
-    )
-  );
+  const xlsxDirectory = path.join(process.cwd(), "pages/api/score");
+  const fileContents = await fs.readFileSync(xlsxDirectory + "/data.xlsx");
+
+  const data = xlsx.parse(fileContents);
   console.log(data[0].data[0]);
   res.status(200).json({ name: JSON.stringify(data[0].data[0]) });
 }
