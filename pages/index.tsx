3b27fbf98e059f5e242672db9db0b84f4a410506
Duong Nguyen <duongital@gmail.com>
2022-08-07 18:10:29 +0700

    add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
+import Logo from "./tennis-logo.png";
+
 const Home: NextPage = () => {
   return (
     <div>
@@ -11,25 +13,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <main className="container mx-auto">
+        <h1 className="text-blue-400">
+          Welcome to Dien dan tennis Bac Trung Nam
         </h1>
 
         <p>Get started by editing </p>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        Powered by{" "}
+        <span>
+          <Image src={Logo} alt="Vercel Logo" width={72} height={72} />
+        </span>
       </footer>
     </div>
   );
