7928001a85448a616a0b0cfc9800d86a1f19b3b9
Duong Nguyen <duongital@gmail.com>
2022-08-07 19:36:52 +0700

    fix: type error

--- pages/api/score/index.ts
+++ pages/api/score/index.ts
@@ -4,18 +4,13 @@ import xlsx from "node-xlsx";
 import path from "path";
 import fs from "fs";
 
-type Data = {
-  name: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
   const xlsxDirectory = path.join(process.cwd(), "pages/api/score");
   const fileContents = await fs.readFileSync(xlsxDirectory + "/data.xlsx");
-
   const data = xlsx.parse(fileContents);
-  console.log(data[0].data[0]);
+
   res.status(200).json(data);
 }
