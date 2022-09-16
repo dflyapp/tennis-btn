911ed0cc17a8d91e11112d58c8bf396ded55e1d0
Duong Nguyen <duongital@gmail.com>
2022-09-16 21:09:51 +0700

    feat: add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
 import "styles/globals.css";
 import "swiper/css/bundle";
+import Script from "next/script";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-4DEJQF554X"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){window.dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-4DEJQF554X');
+    `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
