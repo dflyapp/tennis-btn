6fb87202f1e90e2a0ee069eafc8cd9c23ea05e5d
Duong Nguyen <duongital@gmail.com>
2023-01-13 20:28:30 +0700

    feat: add lang vi

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
+  i18n: {
+    locales: ["vi"],
+    defaultLocale: "vi",
+  },
 };
 
 module.exports = nextConfig;
