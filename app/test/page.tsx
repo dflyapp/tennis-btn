5cb871a6417295f1960b5b0e5e0468c3dc87a279
Duong Nguyen <duongital@gmail.com>
2023-06-11 14:55:45 +0700

    chore: remove prisma

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,31 +1,13 @@
-import prisma from "lib/prisma";
 import { Metadata } from "next";
-
-export const revalidate = 3600; // revalidate every hour
 
 export const metadata: Metadata = {
   title: 'Test Page for Next.js',
 };
 
-async function getData() {
-  const res = await prisma.post.findMany();
-  return res;
-}
-
 export default async function Page() {
-  const data = await getData();
-  console.log(data);
-
   return (
     <>
-      <pre>{JSON.stringify(data)}</pre>
-      <h1>Hello, Next.js!</h1>
-      {data.map((e) => (
-        <>
-          <h1>{e.title}</h1>
-          <p>{e.content}</p>
-        </>
-      ))}
+      <h1>Hello Next App Router 13</h1>
     </>
   );
 }
