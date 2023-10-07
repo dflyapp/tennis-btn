dcffeab1c434dcda5c78469194330f560f61a551
Duong Nguyen <duongital@gmail.com>
2023-10-07 12:54:56 +0700

    chore: update logos

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -1,28 +1,28 @@
-import Image from "next/image";
+import Image from 'next/image'
 
-import Sponsor1 from "assets/sponsors/duc-phat.jpg";
-import Sponsor2 from "assets/sponsors/nam-hai.jpg";
-import Sponsor3 from "assets/sponsors/dong-duong-vina.jpg";
-import Sponsor4 from "assets/sponsors/happy-home.jpg";
-import Sponsor5 from "assets/sponsors/my-pham-anhnhat.jpg";
-import Sponsor6 from "assets/sponsors/tuan-phat.jpg";
-import Sponsor7 from "assets/sponsors/ve-may-bay.jpg";
-import Sponsor8 from "assets/sponsors/tp.jpg";
+import Sponsor1 from 'assets/sponsors/duc-phat.jpg'
+import Sponsor2 from 'assets/sponsors/nam-hai.jpg'
+import Sponsor3 from 'assets/sponsors/co-khi-lien-tuu.jpg'
+import Sponsor4 from 'assets/sponsors/happy-home.jpg'
+import Sponsor5 from 'assets/sponsors/my-pham-anhnhat.jpg'
+import Sponsor6 from 'assets/sponsors/tuan-phat.jpg'
+import Sponsor7 from 'assets/sponsors/ve-may-bay.jpg'
+import Sponsor8 from 'assets/sponsors/tp.jpg'
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Autoplay } from "swiper";
-import Dialog from "./DialogBrand";
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Autoplay } from 'swiper'
+import Dialog from './DialogBrand'
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import Link from "next/link";
+import 'swiper/css'
+import 'swiper/css/pagination'
+import Link from 'next/link'
 
 export default function Sponsors() {
   const sponsorList = [
     {
-      name: "sponsor-1",
-      vnName: "Công ty TNHH SX TM DV & XD Đức Phát",
+      name: 'sponsor-1',
+      vnName: 'Công ty TNHH SX TM DV & XD Đức Phát',
       logo: Sponsor1,
       content: () => (
         <>
@@ -48,64 +48,58 @@ export default function Sponsors() {
         </>
       ),
     },
+    // {
+    //   name: 'sponsor-2',
+    //   vnName: 'Công ty TNHH CB & KD Than Nam Hải',
+    //   logo: Sponsor2,
+    //   content: () => (
+    //     <>
+    //       <h1 className="flex gap-x-2">
+    //         <span>SĐT Liên Hệ:</span>
+    //         <span>0909029898</span>
+    //       </h1>
+    //       <p className="flex gap-x-2">
+    //         <span>Địa chỉ website:</span>
+    //         <Link href="http://thannamhai.com/">thannamhai.com</Link>
+    //       </p>
+    //       <p>
+    //         Công ty TNHH Chế Biến Và Kinh Doanh Than Nam Hải chuyên cung cấp
+    //         than đá Quảng Ninh và than nhập khẩu tới các khách hàng ở khu vực
+    //         phía Nam như TP.HCM, Đồng Nai, Bình Dương, Tây Ninh, Bình Thuận,
+    //         Vũng Tàu, Cần Thơ, An Giang, Long An… Với truyền thống, bề dày kinh
+    //         nghiệm trong lĩnh vực than đá và phương châm kinh doanh Uy tín và
+    //         Chất lượng, tới nay công ty chúng tôi tự hào đã là đối tác cung cấp
+    //         hàng đầu của rất nhiều khách hàng là các nhà máy sản xuất và cả
+    //         những doanh nghiệp kinh doanh than đá ở khu vực phía Nam Sản phẩm
+    //         than đá của chúng tôi cung cấp chuyên phục vụ đốt lò hơi công nghiệp
+    //         cho các nhà máy sản xuất phân bón, xi măng, bê tông, sắt thép, thức
+    //         ăn gia súc, dệt nhuộm, sản xuất giấy… và rất nhiều các ngành công
+    //         nghiệp khác. Quý khách có nhu cầu mua than đá, hãy liên hệ với công
+    //         ty chúng tôi để được tư vấn sử dụng than đá hiệu quả nhất !
+    //       </p>
+    //     </>
+    //   ),
+    // },
     {
-      name: "sponsor-2",
-      vnName: "Công ty TNHH CB & KD Than Nam Hải",
-      logo: Sponsor2,
-      content: () => (
-        <>
-          <h1 className="flex gap-x-2">
-            <span>SĐT Liên Hệ:</span>
-            <span>0909029898</span>
-          </h1>
-          <p className="flex gap-x-2">
-            <span>Địa chỉ website:</span>
-            <Link href="http://thannamhai.com/">thannamhai.com</Link>
-          </p>
-          <p>
-            Công ty TNHH Chế Biến Và Kinh Doanh Than Nam Hải chuyên cung cấp
-            than đá Quảng Ninh và than nhập khẩu tới các khách hàng ở khu vực
-            phía Nam như TP.HCM, Đồng Nai, Bình Dương, Tây Ninh, Bình Thuận,
-            Vũng Tàu, Cần Thơ, An Giang, Long An… Với truyền thống, bề dày kinh
-            nghiệm trong lĩnh vực than đá và phương châm kinh doanh Uy tín và
-            Chất lượng, tới nay công ty chúng tôi tự hào đã là đối tác cung cấp
-            hàng đầu của rất nhiều khách hàng là các nhà máy sản xuất và cả
-            những doanh nghiệp kinh doanh than đá ở khu vực phía Nam Sản phẩm
-            than đá của chúng tôi cung cấp chuyên phục vụ đốt lò hơi công nghiệp
-            cho các nhà máy sản xuất phân bón, xi măng, bê tông, sắt thép, thức
-            ăn gia súc, dệt nhuộm, sản xuất giấy… và rất nhiều các ngành công
-            nghiệp khác. Quý khách có nhu cầu mua than đá, hãy liên hệ với công
-            ty chúng tôi để được tư vấn sử dụng than đá hiệu quả nhất !
-          </p>
-        </>
-      ),
-    },
-    {
-      name: "sponsor-3",
-      vnName: "CÔNG TY TNHH THƯƠNG MẠI KỸ THUẬT ĐÔNG DƯƠNG VI NA",
+      name: 'sponsor-3',
+      vnName: 'CÔNG TY TNHH CƠ KHÍ LIÊN TỰU',
       logo: Sponsor3,
       content: () => (
         <>
           <h1 className="flex gap-x-2">
             <span>SĐT Liên Hệ:</span>
-            <span>090 8809973</span>
+            <span>Đang cập nhật</span>
           </h1>
           <p className="flex gap-x-2">
             <span>Địa chỉ website:</span>
-            <Link href="https://dongduongvina.com.vn/gioi-thieu/gioi-thieu">
-              dongduongvina.com.vn
-            </Link>
-          </p>
-          <p>
-            Nhà phân phối chuyên nghiệp các giải pháp công nghệ trong lĩnh vực
-            chế biến và đóng gói thủy hải sản, thực phẩm, nông sản
+            <span>Đang cập nhật</span>
           </p>
         </>
       ),
     },
     {
-      name: "sponsor-4",
-      vnName: "Công ty Happy Home",
+      name: 'sponsor-4',
+      vnName: 'Công ty Happy Home',
       logo: Sponsor4,
       content: () => (
         <>
@@ -118,8 +112,8 @@ export default function Sponsors() {
       ),
     },
     {
-      name: "sponsor-5",
-      vnName: "Công ty Mỹ Phẩm Anh Nhật",
+      name: 'sponsor-5',
+      vnName: 'Công ty Mỹ Phẩm Anh Nhật',
       logo: Sponsor5,
       content: () => (
         <>
@@ -132,8 +126,8 @@ export default function Sponsors() {
       ),
     },
     {
-      name: "sponsor-6",
-      vnName: "Công ty Tuấn Phát",
+      name: 'sponsor-6',
+      vnName: 'Công ty Tuấn Phát',
       logo: Sponsor6,
       content: () => (
         <>
@@ -146,8 +140,8 @@ export default function Sponsors() {
       ),
     },
     {
-      name: "sponsor-7",
-      vnName: "Công ty Vé Máy Bay Hạnh Phúc",
+      name: 'sponsor-7',
+      vnName: 'Công ty Vé Máy Bay Hạnh Phúc',
       logo: Sponsor7,
       content: () => (
         <>
@@ -160,8 +154,8 @@ export default function Sponsors() {
       ),
     },
     {
-      name: "sponsor-8",
-      vnName: "Công ty Toàn Thịnh Phát",
+      name: 'sponsor-8',
+      vnName: 'Công ty Toàn Thịnh Phát',
       logo: Sponsor8,
       content: () => (
         <>
@@ -178,7 +172,7 @@ export default function Sponsors() {
         </>
       ),
     },
-  ];
+  ]
   return (
     <>
       <Swiper
@@ -207,9 +201,9 @@ export default function Sponsors() {
                 <div className="sponsors">{e.content()}</div>
               </Dialog>
             </SwiperSlide>
-          );
+          )
         })}
       </Swiper>
     </>
-  );
+  )
 }
--- pages/giai-dau/content/long-mekong-2023-lan-2.md
+++ pages/giai-dau/content/long-mekong-2023-lan-2.md
@@ -6,6 +6,6 @@ cover_image: '/giai-dau/2023-11-long-mekong.jpg'
 # hot: true
 ---
 
-![2023-11-long-mekong-detail-1.jpg](https://img.upanh.tv/2023/09/13/2023-11-long-mekong-detail-1.jpg)
+![longmekong-banner-1.jpg](https://img.upanh.tv/2023/10/07/longmekong-banner-1.jpg)
 
-![2023-11-long-mekong-detail-2.jpg](https://img.upanh.tv/2023/09/13/2023-11-long-mekong-detail-2.jpg)
+![longmekong-banner-2.jpg](https://img.upanh.tv/2023/10/07/longmekong-banner-2.jpg)
