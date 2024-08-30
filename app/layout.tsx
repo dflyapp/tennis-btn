340f5d97536727fafa89875dac837b09befeb866
duongital <duongital@gmail.com>
2024-08-30 17:39:29 +0700

    chore: complete layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme="light">
+    <html lang="en" data-theme="mytheme">
       <body className="container mx-auto">{children}</body>
     </html>
   )
--- app/ui/PlayerList.tsx
+++ app/ui/PlayerList.tsx
@@ -1,12 +1,16 @@
 'use client'
 import useSWR from 'swr'
+import useSWRMutation from 'swr/mutation'
 
 const fetcher = (url: string) =>
   fetch(url, { method: 'GET' }).then((res) => res.json())
+const updateName = (url: string) =>
+  fetch(url, { method: 'PUT' }).then((res) => res.json())
 
 export default function PlayerList() {
-  const { data, error, isLoading } = useSWR('/api/players', fetcher)
+  const { data, error, isLoading, mutate } = useSWR('/api/players', fetcher)
   const { players } = data || {}
+  const { trigger } = useSWRMutation('/api/players/1', updateName)
 
   if (error) return <div>failed to load</div>
   if (isLoading) return <div>loading...</div>
@@ -14,25 +18,56 @@ export default function PlayerList() {
   return (
     <div className="container mx-auto">
       <h1>Dashboard</h1>
-      <button
-        disabled={true}
-        className="btn btn-primary"
-        onClick={async () => {}}
-      >
-        update client
-      </button>
-      <div className="flex flex-col">
-        {players?.map((e: any) => {
-          return (
-            <div className="my-2 border w-fit p-2 rounded-md" key={e.id}>
-              <p>
-                {e.id}: {e.name}
-              </p>
-              <p>{e.min}</p>
-              <p>{e.max}</p>
-            </div>
-          )
-        })}
+      <input
+        type="text"
+        placeholder="Tìm tên, điểm"
+        className="input input-bordered w-full max-w-xs my-4"
+      />
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Min</th>
+            <th>Max</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {players?.map((e: any) => {
+            return (
+              <tr key={e.id}>
+                <th>{e.id}</th>
+                <td> {e.name}</td>
+                <td>{e.min}</td>
+                <td>{e.max}</td>
+                <td>
+                  <button
+                    disabled={false}
+                    className="btn btn-secondary"
+                    onClick={() => {}}
+                  >
+                    Chỉnh sửa
+                  </button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+
+      <div className="mt-8">
+        <button
+          disabled={false}
+          className="btn btn-primary"
+          onClick={async () => {
+            await trigger()
+            mutate()
+          }}
+        >
+          Tạo VĐV Mới
+        </button>
       </div>
     </div>
   )
