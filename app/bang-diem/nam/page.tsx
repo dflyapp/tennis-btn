75c50e33001c461cd57bedd4a7b1eb12268f100f
duongital <duongital@gmail.com>
2024-09-03 10:50:08 +0700

    feat: migrate using ssr

---
+++ app/bang-diem/nam/page.tsx
@@ -0,0 +1,30 @@
+import { createClient } from 'utils/supabase/client'
+import { SelectPlayerFemale } from 'db/schema'
+import FilterTable from 'components/FilterTable'
+import { Loading } from 'components'
+import { Header } from 'layouts'
+
+export default async function Page() {
+  const supabase = createClient()
+  const { data, error } = await supabase
+    .from('players_male')
+    .select()
+    .order('id')
+    .returns<SelectPlayerFemale[]>()
+
+  if (error) return <>Đã có lỗi xảy ra</>
+  if (!data) return <Loading />
+
+  if (!data.length)
+    return <span className="text-xs text-gray-400">có lỗi xày ra</span>
+
+  return (
+    <>
+      <Header />
+      <div className="h-20" />
+      <h1 className="text-center my-8">Bảng điểm nam</h1>
+      <FilterTable dataSet={data} />
+      <div className="h-12" />
+    </>
+  )
+}
--- layouts/Footer/index.tsx
+++ layouts/Footer/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image'
 
 import LogoZalo from './logo-zalo.png'
