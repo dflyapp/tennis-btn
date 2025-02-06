51d061ed5b102946a02f8124b261a72469786cd2
duongital <duongital@gmail.com>
2025-02-06 20:47:15 +0700

    remove unused env vars

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,15 +1,17 @@
-import { NodeSDK, logs } from '@opentelemetry/sdk-node'
+import { NodeSDK } from '@opentelemetry/sdk-node'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 
 export function register() {
   const sdk: NodeSDK = new NodeSDK({
-    traceExporter: new OTLPTraceExporter(),
-
-    // send logs to Honeycomb
-    // logRecordProcessor: new logs.SimpleLogRecordProcessor(
-    //   new OTLPLogExporter()
-    // ),
+    serviceName: 'tennis-btn-node',
+    traceExporter: new OTLPTraceExporter({
+      url: 'https://api.honeycomb.io/v1/traces', // US instance
+      //url: "https://api.eu1.honeycomb.io/v1/traces", // EU instance
+      headers: {
+        'x-honeycomb-team': process.env.NEXT_PUBLIC_OTEL_CLIENT || '',
+      },
+    }),
 
     instrumentations: [
       getNodeAutoInstrumentations({
