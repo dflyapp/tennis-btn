dbf0b6730a7df0a764963812d594077dc86db2b7
duongital <duongital@gmail.com>
2025-02-07 20:37:18 +0700

    hide mask on sentry replay

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -11,7 +11,11 @@ Sentry.init({
 
   // Add optional integrations for additional features
   integrations: [
-    Sentry.replayIntegration(),
+    Sentry.replayIntegration({
+      // NOTE: This will disable built-in masking. Only use this if your site has no sensitive data, or if you've already set up other options for masking or blocking relevant data, such as 'ignore', 'block', 'mask' and 'maskFn'.
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
 
     // Supabase integration
     supabaseIntegration(SupabaseClient, Sentry, {
@@ -24,10 +28,23 @@ Sentry.init({
         return !url.startsWith(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest`)
       },
     }),
+
+    // Add browser profiling integration to the list of integrations
+    Sentry.browserProfilingIntegration(),
   ],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
+
+  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/diendantennisbtn\.com/],
+
+  // Set profilesSampleRate to 1.0 to profile every transaction.
+  // Since profilesSampleRate is relative to tracesSampleRate,
+  // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
+  // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
+  // result in 25% of transactions being profiled (0.5*0.5=0.25)
+  profilesSampleRate: 1.0,
 
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
