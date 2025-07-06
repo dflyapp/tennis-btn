8d21d1b2ef05c3f101fe55d916b67de51cb39ea9
Duong Nguyen <duongital@gmail.com>
2025-07-06 10:51:25 +0700

    chore: add loading indicator

--- components/PlayerList/ViewPlayer.tsx
+++ components/PlayerList/ViewPlayer.tsx
@@ -16,13 +16,15 @@ export default function ViewPlayer({
 }: ViewPlayerProps & PropsWithChildren) {
   const supabase = createClient()
   const ref = useRef<HTMLDialogElement>(null)
-  const [log, setLog] = useState<any>([])
+  const [logs, setLogs] = useState<any[]>()
+  const [loading, setLoading] = useState(false)
 
   function closeModal() {
     ref.current?.close()
   }
 
   async function getPlayerLog() {
+    setLoading(true)
     const { data, error } = await supabase
       .from('players_log')
       .select()
@@ -31,7 +33,9 @@ export default function ViewPlayer({
       .order('created_at', { ascending: false })
 
     if (error) alert('Error while getting log')
-    if (data) setLog(data)
+    if (data) setLogs(data)
+
+    setLoading(false)
   }
 
   return (
@@ -46,8 +50,8 @@ export default function ViewPlayer({
         {children}
       </span>
       <dialog ref={ref} id="modal-view-player" className="modal">
-        <div className="modal-box">
-          <div className="flex justify-between">
+        <div className="modal-box min-h-[350px]">
+          <div className='flex justify-between'>
             <h3 className="font-bold text-lg">Lịch sử chỉnh sửa:</h3>
             <button
               onClick={() => {
@@ -58,38 +62,48 @@ export default function ViewPlayer({
               Đóng
             </button>
           </div>
-          {log && log?.length > 0 ? (
-            <table className="table mb-4">
-              <thead>
-                <tr>
-                  <th>Nick Name</th>
-                  <th>Max-Min</th>
-                  <th>Ngày</th>
-                  <th>Giờ</th>
-                </tr>
-              </thead>
-              <tbody>
-                {log?.map((l: any) => (
-                  <tr key={l.id}>
-                    <td>{l.name}</td>
-                    <td>
-                      {l.max}-{l.min}
-                    </td>
-                    <td>
-                      {dayjs(l.created_at).add(7, 'hours').format('DD/MM/YYYY')}
-                    </td>
-                    <td>
-                      {dayjs(l.created_at).add(7, 'hours').format('HH:mm')}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          {loading ? (
+            <div className="flex gap-x-4 py-8">
+              <span className="loading loading-dots loading-md"></span>
+              <span className="loading loading-dots loading-md"></span>
+              <span className="loading loading-dots loading-md"></span>
+              <span className="loading loading-dots loading-md"></span>
+              <span className="loading loading-dots loading-md"></span>
+            </div>
           ) : (
-            <p className="text-left py-4">chưa từng chỉnh điểm</p>
+            <ShowLogs logs={logs} />
           )}
         </div>
       </dialog>
     </>
+  )
+}
+
+function ShowLogs({ logs }: { logs?: any[] }) {
+  if (logs === undefined || logs?.length == 0) {
+    return <p className="text-left py-4">chưa từng chỉnh điểm</p>
+  }
+
+  return (
+    <table className="table mb-4">
+      <thead>
+        <tr>
+          <th>Nick Name</th>
+          <th>Max</th>
+          <th>Min</th>
+          <th>Thời gian</th>
+        </tr>
+      </thead>
+      <tbody>
+        {logs?.map((l) => (
+          <tr key={l.id}>
+            <td>{l.name}</td>
+            <td>{l.max}</td>
+            <td>{l.min}</td>
+            <td>{dayjs(l.createdAt).format('DD/MM/YYYY')}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
